1fc84d4b14285a5e703512428689d8e6625721bf
Andres Botero <boterock@gmail.com>
2018-09-18 07:38:49 -0500

    understanding cpp packaging

--- io_scene_udatasmith/setup.py
+++ io_scene_udatasmith/setup.py
@@ -1,11 +1,14 @@
 
 from distutils.core import setup, Extension
 
-module_datasmith = Extension('datasmith', sources=['datasmith.c'])
+module_datasmith = Extension('datasmith', sources=['datasmith.cpp'])
 
 setup(
-    name='bdatasmith',
+    name='datasmith',
     version='4.20',
     description='demo datasmith package',
-    ext_modules=[module_datasmith]
+    ext_modules=[module_datasmith],
+    url='https://gitlab.com/boterock/blender-datasmith',
+    author='Andres Botero',
+    author_email='boterock@gmail.com',
 )
