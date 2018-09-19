417fc7583921977012e688bd0a59f6e1cd680632
Andres Botero <boterock@gmail.com>
2018-09-19 18:47:38 -0500

    work on exporting meshes

--- io_scene_udatasmith/data_types.py
+++ io_scene_udatasmith/data_types.py
@@ -1,7 +1,7 @@
 import struct
 from xml.etree import ElementTree
 import os
-
+import itertools
 
 def read_array_data(io, data_struct):
 	struct_size = struct.calcsize(data_struct)
@@ -11,6 +11,15 @@ def read_array_data(io, data_struct):
 	data = io.read(count * struct_size)
 	unpacked_data = list(struct.iter_unpack(data_struct, data))
 	return [tup[0] if len(tup) == 1 else tup for tup in unpacked_data ]
+
+def write_array_data(io, struct, data):
+	# first get data length
+	length = len(data)
+	data_struct = '<I' + (struct) * length
+	flat_data = itertools.chain(*data)
+	data = struct.pack(data_struct, *flat_data)
+	io.write(data)
+
 
 def read_data(io, data_struct):
 	struct_size = struct.calcsize(data_struct)
@@ -23,6 +32,17 @@ def read_string(io):
 	count = struct.unpack("<I", io.read(4))[0]
 	data = io.read(count)
 	return data.decode('utf-8').strip('\0')
+
+
+def write_null(io, num_bytes):
+	io.write(b'\0' * num_bytes)
+
+def write_string(io, string):
+	string_bytes = string.encode('utf-8') + '\0'
+	length = len(string_bytes)
+	io.write(struct.pack('<I', length))
+	io.write(string_bytes)
+
 
 
 class UDMesh:
@@ -57,14 +77,42 @@ class UDMesh:
 		self.uvs = []
 
 	def fill_fields(self, bl_mesh):
+		self.init_fields() # start clean
+		import bmesh
+
+		# create copy to triangulate
+		m = bl_mesh.data.copy()
+		bm = bmesh.new()
+		bm.from_mesh(m)
+		bmesh.ops.triangulate(bm, faces=bm.faces[:])
+		# this is just to make sure a UV layer exists
+		bm.loops.layers.uv.verify()
+		bm.to_mesh(m)
+		bm.free()
+
+		# not sure if this is the best way to read normals
+		m.calc_normals_split()
+
+		self.name = bl_mesh.name # get name of the original mesh
+
 		for idx, mat in enumerate(bl_mesh.materials):
 			self.materials[idx] = getattr(mat, 'name', 'DefaultMaterial')
 
-		self.vertices = [0] * len(bl_mesh.vertices) * 3
-		bl_mesh.vertices.foreach_get('co', self.vertices) # this unpacks the vertex data, nothing wrong with that for now
+		for p in m.polygons:
+			self.tris_material_slot.append(p.material_index)
 
+		# no smoothing groups for now
+		self.tris_smoothing_group = [0] * len(self.tris_material_slot)
 
+		for v in m.vertices:
+			self.vertices.append(v.co)
 
+		for l in m.loops:
+			self.triangles.append(l.vertex_index)
+			self.vertex_normals.append(l.normal)
+			self.uvs.append(m.uv_layers[0].data[l.index].uv)
+
+		bpy.data.meshes.remove(m)
 
 	def check_fields(self):
 		assert self.source_models == 'SourceModels'
@@ -88,7 +136,6 @@ class UDMesh:
 			material_map[0] = last_index
 			self.materials[last_index] = 'default_material'
 
-		
 		print(material_map)
 		try:
 			self.tris_material_slot = list(map(lambda x: material_map[x], self.tris_material_slot))
@@ -96,12 +143,10 @@ class UDMesh:
 			print(self.tris_material_slot)
 
 
-
 	def init_with_path(self, path):
 		with open(path, 'rb') as file:
 
-
-			self.a01 = read_data(file, 'II')
+			self.a01 = read_data(file, 'II') # 8 bytes
 			self.name = read_string(file)
 
 			self.a02 = file.read(5)
@@ -120,8 +165,8 @@ class UDMesh:
 			self.vertices = read_array_data(file, "fff")
 			self.triangles = read_array_data(file, "I")
 			
-			self.a05 = read_array_data(file, "I") # not sure about this structure
-			self.a06 = read_array_data(file, "I") # not sure about this structure
+			self.a05 = read_array_data(file, "I") # 4 bytes, not sure about this structure
+			self.a06 = read_array_data(file, "I") # 4 bytes, not sure about this structure
 
 			self.vertex_normals = read_array_data(file, "fff")
 			self.uvs = read_array_data(file, "ff")
@@ -130,13 +175,40 @@ class UDMesh:
 			
 			self.checksum = file.read(16) # I guess... seems to be non deterministic
 			
-			self.a08 = file.read()
+			self.a08 = file.read() #4 bytes more
 			
 			# small check here to crash if something is suspicious
 			assert len(self.triangles) == len(self.uvs)
 			assert len(self.vertex_normals) == len(self.uvs)
 			assert self.a08 == b'\x00\x00\x00\x00' # just to be sure its the end
+		
+	def write_to_path(self, path):
+		with open(path, 'wb') as file:
+			write_null(file, 8)
+			write_string(file, self.name)
+			write_null(file, 5)
+			write_string(file, self.source_models)
+			write_string(file, self.struct_property)
+			write_null(file, 8)
+
+			write_string(file, self.datasmith_mesh_source_model)
 			
+			write_null(file, 49)
+
+			# further analysis revealed:
+			# this loops are per triangle
+			write_array_data(file, 'I', self.tris_material_slot)
+			write_array_data(file, 'I', self.tris_smoothing_group)
+			# per vertex
+			write_array_data(file, 'fff', self.vertices)
+			# per vertexloop
+			write_array_data(file, 'I', self.triangles)
+			write_null(file, 8)
+			write_array_data(file, 'fff', self.vertex_normals)
+			write_array_data(file, 'ff', self.uvs)
+			write_null(file, 36)
+			write_null(file, 16)
+			write_null(file, 4)
 
 class UDMaterial:
 	def __new__(cls, name: str, parent=None, **kwargs):
@@ -197,11 +269,13 @@ class UDActorMesh(UDActor):
 
 
 class UDScene:
-	def __init__(self, path=None, parent=None):
+	def __init__(self, source=None):
 		self.init_fields()
-		if path:
-			self.path = path
-			self.init_with_path(path)
+		if type(source) is str:
+			self.path = source
+			self.init_with_path(self.path)
+		elif source is not None:
+			self.init_with_blend_scene(source)
 
 		self.check_fields() # to test if it is possible for these fields to have different values
 
@@ -241,6 +315,6 @@ class UDScene:
 				UDActorMesh(node=node, parent=self)
 
 		print(self.objects)
-	
-		
- 
+
+	def init_with_blend_scene(self, scene):
+		pass
